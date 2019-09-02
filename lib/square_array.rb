a7f556b834c348ce860d933d99adec783f60d4da
Scott Jordan <scottjordanswimming@gmail.com>
2019-09-02 23:34:43 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,17 @@
 def square_array(array)
-  # your code here
+  numbers = [1, 2, 3]
+  counter = 0
+  new_numbers = Array.new
+  
+  while 
 end
+
+nums = Array.new
+
+nums.push 1
+nums.push 2
+nums.push 3
+nums.push 4
+nums.push 5
+
+puts nums
