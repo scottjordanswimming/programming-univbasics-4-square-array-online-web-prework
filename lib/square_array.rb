ad6e2bf331b044f6363f5bcc2e82077336ddeae1
Scott Jordan <scottjordanswimming@gmail.com>
2019-09-02 23:27:36 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,21 @@
-def square_array(array)
-  # your code here
+def square_array(numbers)
+
+counter = 0 
+numbers = [1, 2, 3]
+
+while numbers[counter]
+    puts numbers[counter] * numbers[counter]
+  counter += 1 
+
+end
+
+puts numbers
+
+
+
+  it 'should square the elements in an array' do
+    expect(square_array([1,2,3])).to eq([1,4,9])
+    expect(square_array([9,10,16,25])).to eq([81,100,256,625])
+  end
+
 end
